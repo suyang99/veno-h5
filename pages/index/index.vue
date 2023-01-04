fd5cc390f038a6e344932dfc929f40241d36199e
荏狸十三 <renshisan@qq.com>
2023-01-04 17:34:10 +0800

    ♻️：初始化项目

---
+++ pages/index/index.vue
@@ -0,0 +1,29 @@
+<template>
+	<view class="content">
+		<uni-card title="标题文字"
+			thumbnail="https://vkceyugu.cdn.bspapp.com/VKCEYUGU-dc-site/460d46d0-4fcc-11eb-8ff1-d5dcf8779628.png"
+			extra="额外信息" note="Tips">
+			内容主体，可自定义内容及样式
+		</uni-card>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+	.content {}
+</style>
